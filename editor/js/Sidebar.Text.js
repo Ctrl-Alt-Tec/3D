5830e7a97eabc417bdc27f2a8ff335f1180d31be
Eduardo Villalpando Mello <5952839+edvilme@users.noreply.github.com>
2019-05-10 14:24:31 -0500

    Update Sidebar.Text.js

--- editor/js/Sidebar.Text.js
+++ editor/js/Sidebar.Text.js
@@ -12,7 +12,6 @@
   var container = new UI.Panel();
   container.setBorderTop( '0' );
 	container.setPaddingTop( '20px' );
-	container.setDisplay( 'none' );
   
   container.add(new UI.Text('qwertyuiop'));
   
