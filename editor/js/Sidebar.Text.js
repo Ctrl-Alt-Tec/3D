72c3176c4eed12b9fdf0d2dcac513b1418c81b97
Eduardo Villalpando Mello <5952839+edvilme@users.noreply.github.com>
2019-05-10 18:51:50 -0500

    Update Sidebar.Text.js

--- editor/js/Sidebar.Text.js
+++ editor/js/Sidebar.Text.js
@@ -14,17 +14,26 @@
 	container.setPaddingTop( '20px' );
   
 	// Edit text
-	
+	container.add(new UI.Text('Text value'));
 	var textRow = new UI.Row();
-	var objectText = new UI.Input().setWidth( '100%' ).setFontSize( '24px' ).onChange( function () {
+	var objectText = new UI.Input().setWidth( '100%' ).setFontSize( '24px' ).onChange( function(){
 		editor.selected.geometry = new THREE.TextGeometry(objectText.getValue(), editor.selected.geometry.parameters.parameters)
 		editor.execute( new AddObjectCommand( editor.selected ) );
 	} );
 	textRow.add(objectText)
-	//
 	container.add(textRow)
+	//
 	 
-  container.add(new UI.Text('qwertyuiop'));
+	container.add(new UI.horizontalRule())
+	 
+	//
+  var fontSizeRow = new UI.Row();
+	var fontSizee = new UI.Number( editor.selected.geometry.parameters.parameters.size ).setWidth('calc(100% - 100px)').onChange( function(){
+		editor.selected.geometry.parameters.parameters.size = fontSizee.getValue();
+		editor.execute(new AddObjectCommand( editor.selected ))
+	} )
+
+	widthRow.add( new UI.Text( strings.getKey( 'sidebar/geometry/box_geometry/width' ) ).setWidth( '90px' ) );
   
   return container;
  }
