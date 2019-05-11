45b67a702947d0d5860db72591be522f50a41d54
Eduardo Villalpando Mello <5952839+edvilme@users.noreply.github.com>
2019-05-10 19:36:54 -0500

    Update Sidebar.Text.js

--- editor/js/Sidebar.Text.js
+++ editor/js/Sidebar.Text.js
@@ -17,14 +17,16 @@
 	var textRow;
 	container.add(new UI.Text('Text value'));
 	editor.signals.objectSelected.add(function(obj){ if(obj!=null && obj.geometry.type == "TextGeometry"){
+		container.remove(textRow)
 		textRow = new UI.Row();
 		var objectText = new UI.Input(editor.selected.geometry.parameters.text).setWidth( '100%' ).setFontSize( '24px' ).onChange( function(){
 			editor.selected.geometry = new THREE.TextGeometry(objectText.getValue(), editor.selected.geometry.parameters.parameters)
 			editor.execute( new AddObjectCommand( editor.selected ) );
 		} );
 		textRow.add(objectText)
+		container.add(textRow)
+		
 	}});
-	container.add(textRow)
 	 
 	 
 	
@@ -35,7 +37,8 @@
 	//
 	var fontSizeRow;
 	editor.signals.objectSelected.add(function(obj){ if(obj!=null && obj.geometry.type == "TextGeometry"){ 	 
-  	fontSizeRow = new UI.Row();
+		container.remove(fontSizeRow);
+		fontSizeRow = new UI.Row();
 		var fontSizee = new UI.Number( editor.selected.geometry.parameters.parameters.size).setWidth('calc(100% - 100px)').onChange( function(){
 			editor.selected.geometry.parameters.parameters.size = fontSizee.getValue();
 			editor.selected.geometry = new THREE.TextGeometry(editor.selected.geometry.parameters.text, editor.selected.geometry.parameters.parameters)
@@ -44,8 +47,8 @@
 
 		fontSizeRow.add( new UI.Text( strings.getKey( 'sidebar/text/fontsize' ) ).setWidth( '90px' ) );
 		fontSizeRow.add(fontSizee);
+		container.add(fontSizeRow);
 	}});
-	container.add(fontSizeRow);
   
   return container;
  }
